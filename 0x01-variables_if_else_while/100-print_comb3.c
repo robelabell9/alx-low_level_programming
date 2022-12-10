fa78e831b002c6c08712977444f333d2f01cdfcc
robelabell9 <robelabella015@gmail.com>
2022-12-10 12:27:30 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: write a program that prints all posiible
- * different combination of two digits
- * Numbers must be separeted by ,, followed by a space
- * The two digits must be differnt
- * you are not allowd to use any variable of tye char
- * All your code should be that main function 
+ *
  * Return: 0
  */
+
 int main(void)
 {
 	int c;
+
 	int d = 0;
+
+	while (d < 10)
 	{
-		while (d < 10)
+		c = 0;
+		while (c < 10)
 		{
-			c = 0;
-			while (c < 10)
+			if (d != c && d < c)
 			{
 				putchar('0' + d);
 				putchar('0' + c);
 
 				if (c + d != 17)
 				{
-					putchar(',');
+					putchar('.');
 					putchar(' ');
 				}
 			}
+
 			c++;
 		}
 		d++;
